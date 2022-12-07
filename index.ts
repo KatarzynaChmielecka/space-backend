0257d14ae723db1fdc8d783576eb09f665120d72
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-12-07 01:49:19 +0100

    Added eslint, pretter, ts configuration. Changed require to import

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-require('dotenv').config();
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
-const bodyParser=require('body-parser');
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-   app.listen(process.env.PORT || 5000)}
-   ).catch((err:string)=>console.log(err))
- 
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch((err: string) => console.log(err));
