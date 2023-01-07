c737b6b38ab2e0186bc26b4b24eeb6f14b98ae86
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-01-07 23:11:43 +0100

    Removed avatar from patchUserData, added access-control-allow-methods

--- index.ts
+++ index.ts
@@ -35,6 +35,7 @@ app.use(function (_req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   next();
 });
 app.use(bodyParser.json());
