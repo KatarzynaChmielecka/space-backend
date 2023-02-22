b5ed38300be6979f6769f129efdc42c60cb3b106
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-02-22 16:21:08 +0100

    test for deploy7

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import passport from 'passport';
@@ -8,8 +9,6 @@ import { Strategy as LocalStrategy } from 'passport-local';
 
 import userRoutes from './src/routes/user-routes';
 import UserModel, { UserDoc } from './src/models/user';
-
-dotenv.config();
 
 const app = express();
 
@@ -24,7 +23,10 @@ declare global {
 }
 
 app.use(function (_req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://space-backend-26zw.onrender.com');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://space-backend-26zw.onrender.com',
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
