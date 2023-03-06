7616d93f52de372711d9e80256a31b0d5c1819d1
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-03-06 21:43:26 +0100

    Added  patch-name-validator

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -18,6 +18,7 @@ import { fileUpload } from '../middlewares/file-upload';
 import { loginValidation } from '../controllers/login-validator';
 import { userValidation } from '../controllers/user-validator';
 import { patchPasswordValidation } from '../controllers/patch-password-validator';
+import { patchNameValidation } from '../controllers/patch-name-validator';
 
 const router = Router();
 
@@ -29,7 +30,7 @@ router.post(
 );
 router.post('/login', loginValidation, loginUser);
 router.post('/:id/images', fileUpload.single('images'), auth, postImage);
-router.patch('/:id/name', auth, patchUserName);
+router.patch('/:id/name', auth, patchNameValidation, patchUserName);
 router.patch('/:id/email', auth, patchUserEmail);
 router.patch('/:id/password', auth, patchPasswordValidation, patchUserPassword);
 router.patch('/:id/image', fileUpload.single('avatar'), auth, patchAvatar);
